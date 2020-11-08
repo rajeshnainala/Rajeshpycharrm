8e875dbbdedd6be43c49d5d6c83800e59318ac0d
rajeshnainala <74012231+rajeshnainala@users.noreply.github.com>
2020-11-08 10:33:43 +0530

    Create Array list.py
    Program for Array list

---
+++ Array list.py
@@ -0,0 +1,5 @@
+input=[-2,-6,4,1,8,9]
+Sorted_Array=sorted(input)
+print("The largest number in the Array list is:",(Sorted_Array[5:]))
+print(" The Smallest number in th Array List is:",(Sorted_Array[:1]))
+
