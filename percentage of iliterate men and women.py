62afad32441ee64fec1fcca4ed1d753ac44a64ec
rajeshnainala <74012231+rajeshnainala@users.noreply.github.com>
2020-11-10 15:48:00 +0530

    Update percentage of iliterate men and women.py
    costprice of single unit is updated

--- percentage of iliterate men and women.py
+++ percentage of iliterate men and women.py
@@ -12,3 +12,10 @@ print("Number of Men who are illiterate :",illiterate_men)
 print("Number of Women who are illiterate: ",illiterate_women)
 print("Total No of illiterate are: ",total_illiterate)
 
+#Cost price of single unit
+sellingprice_15=int(input("Enter the selling price for 15 units "))
+profit_15=int(input("Enter the value of profit for 15 items"))
+costprice_15=sellingprice_15-profit_15
+costprice_singleunit=costprice_15/15
+print("cost price of a single unitis :",round(costprice_singleunit,2))
+
