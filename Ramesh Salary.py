178eb79e17576211ee87e6f931e37df4303d754a
rajeshnainala <74012231+rajeshnainala@users.noreply.github.com>
2020-11-10 10:37:02 +0530

    Create Ramesh Salary.py
    Program one in chapter one

---
+++ Ramesh Salary.py
@@ -0,0 +1,12 @@
+
+basic_salary=int(input("enter the amount"))
+dearness_allowance=(40/100)*basic_salary
+homerent_allowance=(20/100)*basic_salary
+grosspay=basic_salary+dearness_allowance+homerent_allowance
+print("Ramesh Pay Slip")
+print("*********************************")
+print("  Basic Salary      :",basic_salary)
+print("Dearness Allowance  :",dearness_allowance)
+print("Home Rent Allowance :",homerent_allowance)
+print("*******************************")
+print("  Total Gross Pay   :",grosspay)
