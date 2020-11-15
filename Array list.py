b89ed47a0571e3d8fbb8755602230d2dbcea3fa6
rajeshnainala <74012231+rajeshnainala@users.noreply.github.com>
2020-11-15 08:50:08 +0530

    Update Array list.py

--- Array list.py
+++ Array list.py
@@ -1,5 +1,12 @@
-input=[-2,-6,4,1,8,9]
-Sorted_Array=sorted(input)
-print("The largest number in the Array list is:",(Sorted_Array[5:]))
-print(" The Smallest number in th Array List is:",(Sorted_Array[:1]))
+import array as arr
+unsorted_array=arr.array('i',[3,-6,7,10,33,2,-9,-13,15])
+max=unsorted_array[0]
+min=unsorted_array[0]
+for i in range(0,len(unsorted_array)):
+    if max<unsorted_array[i]:
+        max=unsorted_array[i]
+    if min>unsorted_array[i]:
+        min=unsorted_array[i]
+print("the largest number in array is",max)
+print("the smallest number in array is ",min)
 
