c3867a48bcd03e7a5a6a74e8af40736fb4056893
rajeshnainala <74012231+rajeshnainala@users.noreply.github.com>
2021-01-30 19:37:32 +0530

    Create matrix puzzle.py

---
+++ letusc_chapter8/matrix puzzle.py
@@ -0,0 +1,12 @@
+import numpy as np
+import random
+a=np.array([[2,8,10,13],[5,9,12,1],[4,11,15,7],[3,0,14,6]],int)
+print(a)
+for i in range(10):
+    key=int(input("enter value between 1 and 4"))
+    if key==1:
+        a
+
+
+
+
